24003d5ae6d8dd9d296daefc9b1fce7de9802103
Vitor Hugo <vitorh.7@outlook.com>
2023-02-14 16:45:52 -0300

    Exemplos da aula 1

---
+++ type-boolean.js
@@ -0,0 +1,12 @@
+const primeiroNumero = 5;
+const segundoNumero = 5;
+
+//console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "alura";
+const texto2 = "ALURA";
+const texto3 = texto2.toLowerCase();
+
+console.log(texto1 === texto2.toLowerCase());
+
+//.length para meçar algo tamanho de algo
---
+++ type-string.js
@@ -0,0 +1,15 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = 'O leo disse "oi!"';
+console.log(citacao);
+
+// concatenação (+)
+
+const soma1 = "Oi";
+const soma2 = " Juliana";
+console.log(soma1 + soma2);
+
+//template string ou template literal
