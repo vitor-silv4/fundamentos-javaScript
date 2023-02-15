781e12c7901a015fa7958d74bcfe8f90ec502a1b
Vitor Hugo <vitorh.7@outlook.com>
2023-02-14 22:44:58 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+// tipo de dados
+// booleans
+
+
+// conversão implícita
+const numero = 456;
+const numeroString = "456";
+
+//Number()
+//String()
+console.log(numero + Number(numeroString))
+
+// conversão explícita
---
+++ truthy-falsy.js
@@ -0,0 +1,20 @@
+// boolean
+/* const usuarioLogado = true;
+const contaPaga = false; */
+
+// truty ou falsy
+
+// 0 => false
+// 1 => false
+
+/* console.log(0 == false);
+console.log('' == false);
+console.log(1 == true); */
+
+
+let minhaVar;
+let varNull = null;
+let numero = 3;
+let texto = "Alura";
+console.log(typeof minhaVar)
+console.log(typeof varNull)
